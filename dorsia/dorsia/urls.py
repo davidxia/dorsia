24be8d67ffb9d734f533079b3c2bbc8d3a260553
David Xia <david@davidxia.com>
2012-04-08 15:11:41 -0400

    Added Bootstrap and setup urls with views.
    Added static files and templates

--- dorsia/dorsia/urls.py
+++ dorsia/dorsia/urls.py
@@ -4,14 +4,15 @@ from django.conf.urls import patterns, include, url
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns("",
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    # url(r"^admin/doc/", include("django.contrib.admindocs.urls")),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    # url(r"^admin/", include(admin.site.urls)),
 
-    url( r'^$', 'dorsianyc.views.home', name = 'home' ),
-    # url(r'^dorsia/', include('dorsia.foo.urls')),
+    url( r"^$", "dorsianyc.views.home", name = "home" ),
+    url( r"^menu$", "dorsianyc.views.menu", name = "menu" ),
+    url( r"^reservations$", "dorsianyc.views.reservations", name = "reservations" ),
 
 )
--- dorsia/dorsianyc/views.py
+++ dorsia/dorsianyc/views.py
@@ -3,5 +3,20 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response
 
 def home( request ):
-    return render_to_response( "home.html", context_instance = RequestContext( request ) )
+    return render_to_response(
+        "home.html",
+        dict( currentPage = "home" ),
+        context_instance = RequestContext( request ) )
+
+def menu( request ):
+    return render_to_response(
+        "menu.html",
+        dict( currentPage = "menu" ),
+        context_instance = RequestContext( request ) )
+
+def reservations( request ):
+    return render_to_response(
+        "reservations.html",
+        dict( currentPage = "reservations" ),
+        context_instance = RequestContext( request ) )
 
