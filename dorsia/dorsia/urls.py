61e7dbf4e11ee1e6308e5eff5334d73674f55c0f
David Xia <david@davidxia.com>
2012-04-09 23:28:31 -0400

    Enabled admin

--- dorsia/dorsia/urls.py
+++ dorsia/dorsia/urls.py
@@ -1,16 +1,14 @@
 from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns("",
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r"^admin/doc/", include("django.contrib.admindocs.urls")),
 
-    # Uncomment the next line to enable the admin:
-    # url(r"^admin/", include(admin.site.urls)),
-
+    url( r"^admin/", include(admin.site.urls)),
     url( r"^$", "dorsianyc.views.home", name = "home" ),
     url( r"^menu$", "dorsianyc.views.menu", name = "menu" ),
     url( r"^reservations$", "dorsianyc.views.reservations", name = "reservations" ),
